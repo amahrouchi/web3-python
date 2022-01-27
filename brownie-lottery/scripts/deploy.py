e7733ac768be0b5ae3af5845bba60fd8eb99b96b
Angelo Mahrouchi <angelo.mahrouchi@redlab.io>
2022-01-27 22:34:43 +0100

    build(brownie Lottery): call all the Lottery contract features to start and end a complete lottery

--- brownie-lottery/scripts/deploy.py
+++ brownie-lottery/scripts/deploy.py
@@ -1,10 +1,10 @@
 from brownie import accounts, network, config, Lottery
-from scripts.utils import get_account, get_contract
-
+from scripts.utils import get_account, get_contract, fund_with_link
+import time
 
 def deploy_lottery():
     account = get_account()
-    lottery = Lottery.deploy(
+    Lottery.deploy(
         get_contract("eth-usd-price-feed").address,
         get_contract("vrf-coordinator").address,
         get_contract("link-token").address,
@@ -16,5 +16,51 @@ def deploy_lottery():
     print("Deployed lottery!")
 
 
+def start_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+
+    # Il vaut mieux toujours après une transaction attendre qu'elle ait été validé par la blockchain
+    # sinon on peut rencontrer des erreurs. Dans mon cas, je n'en ai pas eu mais le mec dans le tuto oui
+    # avec une histoire d'erreur liée au statut de la transaction probablement
+    # https://www.youtube.com/watch?v=M576WGiDBdQ à 7h36m12s
+    starting_tx = lottery.startLottery({"from": account})
+    starting_tx.wait(1)
+    print("Lottery started!")
+
+
+def enter_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+
+    # Ici on récupère la valeur minimale pour participer à la loterie
+    value = lottery.getEntranceFee() + 1000  # On ajoute qq WEI pour être sûr d'être au dessus de la valeur minimale
+
+    # On lance la transaction en précisant le compte participant à la loterie et la valeur de la participation
+    tx = lottery.enter({"from": account, "value": value})
+    tx.wait(1)
+    print("You entered the lottery!")
+
+def end_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    link_tx = fund_with_link(lottery.address)
+    link_tx.wait(1)
+
+    end_tx = lottery.endLottery({"from": account})
+    end_tx.wait(1)
+    print(f"The lottery has ended! Waiting for the winner to be determinated...")
+
+    # Ici on attend la réponse du noeud chainlink à notre demande de nombre aléatoire
+    time.sleep(60)
+    print(f"Winner is {lottery.recentWinner()}") # Les attributs publics d'un contrat doivent être appelé avec des ()
+
+    # Sur une blockchain locale type Ganache, il n'y a pas de noeud ChainLink qui répondra à notre appel
+    # via la méthode fulfillRandomness de notre contrat de Lottery du coup aucun gagnant ne peut être déterminé
+
+
 def main():
     deploy_lottery()
+    start_lottery()
+    enter_lottery()
+    end_lottery()
--- brownie-lottery/scripts/utils.py
+++ brownie-lottery/scripts/utils.py
@@ -1,5 +1,5 @@
-from brownie import network, accounts, config, MockV3Aggregator, VRFCoordinatorMock, LinkToken, Contract
-from web3 import Web3
+from brownie import network, accounts, config, MockV3Aggregator, VRFCoordinatorMock, LinkToken, Contract, interface
+# from web3 import Web3
 
 # Constantes
 FORKED_LOCAL_ENV = ["mainnet-fork", "mainnet-fork-dev"]
@@ -47,7 +47,9 @@ def get_contract(contract_name):
         if len(contract_type) <= 0:
             deploy_mocks()
         contract = contract_type[-1]
-    # Sinon on récupè l'adresse depuis la config et on instancie le contract depuis la blockchain
+    # Sinon on récupère l'adresse depuis la config et on instancie le contract depuis la blockchain
+    # et on connait sont ABI grace aux mocks que l'on a créé (l'ABI d'un mock et du vrai contrat est la même,
+    # c'est le principe d'un mock en fait)
     else:
         contract_address = config["network"][network.show_active()][contract_name]
         contract = Contract.from_abi(
@@ -66,3 +68,34 @@ def deploy_mocks(decimals=DECIMALS, initial_value=STARTING_PRICE):
     link_token = LinkToken.deploy({"from": account})
     VRFCoordinatorMock.deploy(link_token.address, {"from": account})
     print("Mocks deployed!")
+
+
+def fund_with_link(contract_address, account=None, link_token=None, amount=100000000000000000):  # 0.1 LINK
+    account = account if account else get_account()
+
+    # Manière 1 de recup un contrat.
+    # On rappelle que get_contract() utilise Contract.from_abi() pour créer le contrat
+    # cette méthode demande de connaitre l'ABI du contract et son adresse
+    # ===================================================================
+    link_token = link_token if link_token else get_contract("link-token")
+    tx = link_token.transfer(contract_address, amount, {"from": account})
+    # ===================================================================
+
+    # Manière 2 de récupérer un contract
+    # Via `interface` dans brownie + l'addresse du contrat en question
+    # l'interface sera compilé en une ABI de contrat utilisable
+    # et le contrat sera créé à partir de cette ABI et de l'adresse du contrat sur la blockchain
+    # ===================================================================
+    # link_token = interface.LinkTokenInterface(link_token.address)
+    # tx = link_token.transfer(contract_address, amount, {"from": account})
+    # ===================================================================
+
+    # Quelques commentaires sur ces 2 méthode
+    # De ce que je comprends, l'adresse du contrat ne suffit pas probablement parce que c'est juste
+    # du binaire stocké sur une adresse dans la blockchain. Par contre, avec l'ABI en plus de l'adresse,
+    # on connait la carte qui nous permet d'exploiter ce binaire de manière efficace
+
+    tx.wait(1)
+    print("Contract funded with some LINK!")
+
+    return tx
