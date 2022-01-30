d523676b010506c97238f90b5f0d3221ed6fc37d
Angelo Mahrouchi <angelo.mahrouchi@redlab.io>
2022-01-30 11:57:30 +0100

    doc(erc token): token deployed on the local Ganache blockchain

---
+++ erc20-token/scripts/deploy.py
@@ -0,0 +1,17 @@
+from brownie import accounts, network, config, OurToken
+from scripts.utils import TOKEN_SUPPLY, get_account
+
+
+def deploy():
+    account = get_account()
+    our_token = OurToken.deploy(
+        TOKEN_SUPPLY,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify", False)
+    )
+    print("Token deployed!")
+    return our_token
+
+
+def main():
+    deploy()
